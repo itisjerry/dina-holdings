5b64818c0c789e28d4b460444ed8f84ccbad34f7
Yahya Malik <yahyamalik.ofc@gmail.com>
2025-11-07 14:39:34 -0500

    initial push

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' }
-    ]
-  }
+  async redirects() {
+    return [
+      { source: '/Privacy', destination: '/privacy', permanent: true },
+    ];
+  },
 };
-export default nextConfig;
+module.exports = nextConfig;
